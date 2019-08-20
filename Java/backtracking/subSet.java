a923cdfa6a5b301d115e74c1044d68791c26085d
Alicia <37165165+YaqianQi@users.noreply.github.com>
2019-08-20 10:42:19 -0400

    Add files via upload

--- Java/backtracking/subSet.java
+++ Java/backtracking/subSet.java
@@ -1,1 +1,41 @@
+package backtracking;
+import java.util.*;
+public class subSet {
+	public static List<List<Integer>> findSubSet(int[] nums) {
+		List<List<Integer>> res = new ArrayList<>();
+		if(nums == null || nums.length == 0) {
+			return res;
+		}
+		
+		Arrays.parallelSort(nums);
+		List<Integer> temp = new ArrayList<>();
+		helper(nums, res, temp, 0);
+		return res;
+	}
+	
+	public static void helper(int[] nums, List<List<Integer>> res, List<Integer> subset, int startIdx) {
+		res.add(new ArrayList<>(subset));
+		
+		for(int i = startIdx; i < nums.length ; i++) {
+			subset.add(nums[i]);
+			helper(nums, res,subset,i+1);
+			subset.remove(subset.size() - 1);
+		}
+		
+		
+	}
+	
+	
+	public static void main(String [] args) {
+		int[] nums  = {1,2,3};
+		
+		List<List<Integer>> res =  findSubSet(nums);
+		for(List<Integer> res2 : res) {
+			System.out.println(res2);
+		}
+		
+		
+ 	}
+	
 
+}
