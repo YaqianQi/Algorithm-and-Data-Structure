73752582cbd6a0a099f9545b79990bf32edf8edb
Alicia <37165165+YaqianQi@users.noreply.github.com>
2019-08-23 08:45:02 -0400

    Add files via upload

---
+++ Java/Stack/71.simplifyPath.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class simplifyPath {
+	
+	public String simplifyPathMethod(String path) {
+		Stack<String> stack = new Stack<>();
+		
+		String[] paths = path.split("/+");
+		for(String s: paths) {
+			if(s.equals("..")){
+				if(!stack.isEmpty()) {
+					stack.pop();
+				}
+			}else if(!s.equals(".") && !s.equals("")) {
+				stack.push(s);
+			}
+		}
+		
+		String res = "";
+		while(!stack.isEmpty()) {
+			res = '/' + stack.pop() + res;
+		}
+		
+		if(res.length() == 0) {
+			return "/";
+		}
+		return res;
+		
+		}
+	
+	public static void main(String[] args) {
+		String path = "/home/123";
+		
+		simplifyPath  sm  = new simplifyPath();
+		System.out.println(sm.simplifyPathMethod(path));
+	}
+
+}
