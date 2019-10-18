2cd926a20eb0160b619eb1337540a46ed68c0fd1
Alicia <37165165+YaqianQi@users.noreply.github.com>
2019-10-17 20:59:39 -0400

    Update _253_meetingRoomsII.java

--- Java/priorityQueue/_253_meetingRoomsII.java
+++ Java/priorityQueue/_253_meetingRoomsII.java
@@ -55,4 +55,28 @@ public class _253_meetingRoomsII {
 	        return q.size();
 	      
 	    }
+	
+	
+	public int minMeetingRooms(int[][] intervals) {
+	    Arrays.sort(intervals, Comparator.comparing((int[] itv) -> itv[0]));
+
+	    PriorityQueue<Integer> heap = new PriorityQueue<>();
+	    int count = 0;
+	    for (int[] itv : intervals) {
+		if (heap.isEmpty()) {
+		    count++;
+		    heap.offer(itv[1]);
+		} else {
+		    if (itv[0] >= heap.peek()) {
+			heap.poll();
+		    } else {
+			count++;
+		    }
+
+		    heap.offer(itv[1]);
+		}
+	    }
+
+	    return count;
+	}
 }
