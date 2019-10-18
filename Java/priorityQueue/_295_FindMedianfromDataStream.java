f210df94a401a58f35f7192816aca39b84986771
Alicia <37165165+YaqianQi@users.noreply.github.com>
2019-10-17 22:24:41 -0400

    Add files via upload

---
+++ Java/priorityQueue/_295_FindMedianfromDataStream.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class _295_FindMedianfromDataStream {
+	PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+	PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+	public _295_FindMedianfromDataStream() {
+	        
+	    }
+	    
+	    public void addNum(int num) {
+	        minHeap.offer(num);
+	        maxHeap.offer(minHeap.poll());
+	        if (maxHeap.size() > minHeap.size()) {
+	        	minHeap.offer(maxHeap.poll());
+	        }
+	    }
+	    
+	    public double findMedian() {
+	        if(minHeap.size()>maxHeap.size()) {
+	        	return minHeap.poll();
+	        }else {
+	        	return 1/2 * (minHeap.poll()+ maxHeap.poll());
+	        }
+	    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		_295_FindMedianfromDataStream fm = new _295_FindMedianfromDataStream ();
+		fm.addNum(3);
+		fm.addNum(4);
+		fm.addNum(5);
+		System.out.println(fm.findMedian());
+
+	}
+
+}
