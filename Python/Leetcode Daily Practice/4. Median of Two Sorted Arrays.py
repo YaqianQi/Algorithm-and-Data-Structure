1b8566ad78a77a72906f11c5671a53958372f4da
Yaqian Qi <alicia.qyq@gmail.com>
2020-06-14 23:39:23 -0400

    2020-06-14 sunday pratice median sorted array

---
+++ Python/Leetcode Daily Practice/4. Median of Two Sorted Arrays.py
@@ -0,0 +1,49 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        
+        A, B = nums1, nums2
+        m, n = len(A), len(B)
+        if m > n:
+            A, B, m, n = B, A, n, m
+        if n == 0:
+            raise ValueError
+
+        imin, imax, half_len = 0, m, (m + n + 1) / 2
+        while imin <= imax:
+            i = int((imin + imax) / 2)
+            j = int(half_len - i)
+            print(f"i:{i}, j:{j}, half_len:{half_len}, a left: {A[:i]}, a right:{A[i:]}, b left:{B[:j]}, b right:{B[j:]}")
+            if i < m and B[j-1] > A[i]:
+                # i is too small, must increase it
+                imin = i + 1
+            elif i > 0 and A[i-1] > B[j]:
+                # i is too big, must decrease it
+                imax = i - 1
+            else:
+                # i is perfect
+
+                if i == 0: max_of_left = B[j-1]
+                elif j == 0: max_of_left = A[i-1]
+                else: max_of_left = max(A[i-1], B[j-1])
+
+                if (m + n) % 2 == 1:
+                    return max_of_left
+
+                if i == m: min_of_right = B[j]
+                elif j == n: min_of_right = A[i]
+                else: min_of_right = min(A[i], B[j])
+
+                return (max_of_left + min_of_right) / 2.0
+
+if __name__=="__main__":
+    x = [1,3,5,6]
+    y = [2,4,7,8,9]
+    sol = Solution()
+    # exp = 
+    res = sol.findMedianSortedArrays(x, y)
+    print(res)
